6810eb5df6d146ed1d3e711ad71c49c5793c51c0
lokathor <zefria@gmail.com>
2023-11-26 02:16:52 -0700

    one helper fn complete.

---
+++ src/lib.rs
@@ -0,0 +1,48 @@
+#![no_std]
+#![allow(unused_mut)]
+#![allow(non_camel_case_types)]
+
+use core::mem::MaybeUninit;
+
+pub type mu_u8 = MaybeUninit<u8>;
+pub type mu_u16 = MaybeUninit<u16>;
+pub type mu_u32 = MaybeUninit<u32>;
+
+/// Copies `count` bytes from `src` to `dest`, going upward in address value.
+///
+/// ## Safety
+/// * If `count` is zero, the `src` and `dest` pointers are not accessed, and
+///   they can even be invalid or null.
+/// * If `count` is non-zero, then both `src` and `dest` must be valid for
+///   `count` bytes forward and one of the following must be true:
+///   * The `src` and `dest` regions are entirely disjoint.
+///   * `src` equals `dest` (there is exact overlap).
+///   * `src` is *greater* than `dest` (a partial overlap).
+#[inline]
+#[cfg_attr(feature = "link_iwram", link_section = ".iwram.copy_u8_forward")]
+pub unsafe extern "C" fn copy_u8_forward(
+  mut dest: *mut mu_u8, mut src: *const mu_u8, mut count: usize,
+) {
+  unsafe {
+    #[cfg(not(all(target_arch = "arm", feature = "arm7tdmi")))]
+    while count > 0 {
+      *dest = *src;
+      dest = dest.add(1);
+      src = src.add(1);
+      count -= 1;
+    }
+    #[cfg(all(target_arch = "arm", feature = "arm7tdmi"))]
+    core::arch::asm! {
+      "1:",
+      "subs    {count}, {count}, #1",
+      "ldrbge  {temp}, [{src}], #1",
+      "strbge  {temp}, [{dest}], #1",
+      "bgt     1b",
+      dest = inout(reg) dest => _,
+      src = inout(reg) src => _,
+      count = inout(reg) count => _,
+      temp = out(reg) _,
+      options(nostack)
+    }
+  }
+}
---
+++ tests/all_tests.rs
@@ -0,0 +1,107 @@
+use aeabi_fns::copy_u8_forward;
+
+#[allow(dead_code)]
+fn rand_bytes(n: usize) -> Vec<u8> {
+  let mut v = vec![0; n];
+  getrandom::getrandom(&mut v).unwrap();
+  v
+}
+
+#[allow(dead_code)]
+fn rand_halfwords(n: usize) -> Vec<u16> {
+  let mut v = vec![0_u16; n];
+  getrandom::getrandom(bytemuck::cast_slice_mut(&mut v)).unwrap();
+  v
+}
+
+#[allow(dead_code)]
+fn rand_words(n: usize) -> Vec<u32> {
+  let mut v = vec![0_u32; n];
+  getrandom::getrandom(bytemuck::cast_slice_mut(&mut v)).unwrap();
+  v
+}
+
+fn rand_u32() -> u32 {
+  let mut bytes = [0; 4];
+  getrandom::getrandom(&mut bytes).unwrap();
+  u32::from_ne_bytes(bytes)
+}
+
+struct Lcg(u32);
+impl Lcg {
+  fn new() -> Self {
+    Self(rand_u32())
+  }
+  fn next_u32(&mut self) -> u32 {
+    self.0 = self.0.wrapping_mul(747796405).wrapping_add(1);
+    self.0
+  }
+}
+
+#[test]
+fn test_copy_forward_u8() {
+  let mut lcg = Lcg::new();
+
+  // disjoint regions is the basic use case
+  for len in 0..=16_usize {
+    let src = rand_bytes(64);
+    for s in 0..len {
+      for d in 0..len {
+        let mut dest_expected = vec![0; 64];
+        let mut dest_actual = vec![0; 64];
+        let b = (lcg.next_u32() % 16) as usize;
+        let d_start = b + d;
+        let d_end = d_start + len;
+        let s_start = b + s;
+        let s_end = s_start + len;
+        dest_expected[d_start..d_end].copy_from_slice(&src[s_start..s_end]);
+        unsafe {
+          copy_u8_forward(
+            dest_actual.as_mut_ptr().add(d_start).cast(),
+            src.as_ptr().add(s_start).cast(),
+            len,
+          )
+        }
+        assert_eq!(dest_expected, dest_actual);
+      }
+    }
+  }
+
+  // src == dest is allowed for simplicity (but has no effect)
+  for len in 0..=16_usize {
+    let base = rand_bytes(64);
+    for s in 0..len {
+      let mut new = base.clone();
+      unsafe {
+        let p = new.as_mut_ptr();
+        copy_u8_forward(p.add(s).cast(), p.add(s).cast(), len)
+      }
+      assert_eq!(base, new);
+    }
+  }
+
+  // src > dest works even when the regions overlap
+  for len in 3..=16_usize {
+    let src = rand_bytes(64);
+    for s in 0..len {
+      for d in 0..len {
+        if s < d {
+          continue;
+        }
+        let mut out_expected = src.clone();
+        let mut out_actual = src.clone();
+        let b = (lcg.next_u32() % 16) as usize;
+        let d_start = b + d;
+        //let d_end = d_start + len;
+        let s_start = b + s;
+        let s_end = s_start + len;
+        out_expected.copy_within(s_start..s_end, d_start);
+        unsafe {
+          let p = out_actual.as_mut_ptr();
+          copy_u8_forward(p.add(d_start).cast(), p.add(s_start).cast(), len)
+        }
+        assert_eq!(out_expected, out_actual);
+      }
+    }
+  }
+}
