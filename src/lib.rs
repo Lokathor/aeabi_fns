f5c7a159e6d7af44d5a31525352abe5808181dc1
lokathor <zefria@gmail.com>
2023-11-26 15:27:49 -0700

    copy_u16_forward and copy_u16_backward

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![no_std]
+//#![no_std]
 #![allow(unused_mut)]
 #![allow(non_camel_case_types)]
 #![allow(unsafe_op_in_unsafe_fn)]
@@ -9,12 +9,24 @@ pub type mu_u8 = MaybeUninit<u8>;
 pub type mu_u16 = MaybeUninit<u16>;
 pub type mu_u32 = MaybeUninit<u32>;
 
+/// This macro makes it easy to pick between `armv4t`-specific inline asm and
+/// the Rust-only fallback version.
 macro_rules! cfg_armv4t {
-  ( yes: { $($yes_tokens:tt)* } no: { $($no_tokens:tt)* } ) => {
-    #[cfg(all(target_arch="arm", feature="armv4t"))]{
+  (
+    // this is approximately how I expect you'd format the macro invocation
+    yes: {
+      $($yes_tokens:tt)*
+    }
+    no: {
+      $($no_tokens:tt)*
+    }
+  ) => {
+    #[cfg(all(target_arch="arm", feature="armv4t"))]
+    {
       $($yes_tokens)*
     }
-    #[cfg(not(all(target_arch="arm", feature="armv4t")))]{
+    #[cfg(not(all(target_arch="arm", feature="armv4t")))]
+    {
       $($no_tokens)*
     }
   }
@@ -41,8 +53,8 @@ pub unsafe extern "C" fn copy_u8_forward(
 ) {
   cfg_armv4t! {
     yes: {
-      // This loop assumes that the count is non-zero to start, and so it does
-      // the subtract followed by a conditional copy and continue.
+      // This loop assumes that the count is non-zero to start, and so it always
+      // updates `count`, followed by a conditional copy and continue.
       // * Pro: 8 bytes less code in the binary
       // * Pro: save 2 cycles on non-zero sized copies
       // * Con: lose 3 cycles on zero sized copies.
@@ -60,15 +72,71 @@ pub unsafe extern "C" fn copy_u8_forward(
       }
     }
     no: {
-      // When we write the loop as Rust, LLVM will generally test for 0 and
-      // early return, then copy and subtract and continue.
-      while count > 0 {
+      while count >= 1 {
         *dest = *src;
         dest = dest.add(1);
         src = src.add(1);
         count -= 1;
       }
     }
+  }
+}
+
+/// Copies `count` bytes from `src` to `dest`, going upward in address value.
+///
+/// Copies are done in 2-byte chunks as much as possible. If the number of bytes
+/// to copy is odd then the last copy will be a single-byte copy.
+///
+/// ## Safety
+/// * If `count` is zero, the `src` and `dest` pointers are not accessed, and
+///   they can even be invalid or null.
+/// * If `count` is non-zero, then both `src` and `dest` must be aligned, valid
+///   for `count` bytes forward, and one of the following must be true:
+///   * The `src` and `dest` regions are entirely disjoint.
+///   * `src` equals `dest` (there is exact overlap).
+///   * `src` is *greater* than `dest` (a partial overlap).
+#[inline]
+#[cfg_attr(feature = "link_iwram", link_section = ".iwram.copy_u8_forward")]
+#[cfg_attr(
+  all(target_arch = "arm", target_feature = "thumb-mode", feature = "armv4t"),
+  instruction_set(arm::a32)
+)]
+pub unsafe extern "C" fn copy_u16_forward(
+  mut dest: *mut mu_u16, mut src: *const mu_u16, mut count: usize,
+) {
+  eprintln!("=== start count: {count}");
+  cfg_armv4t! {
+    yes: {
+      // The loop reasoning here is similar to `copy_u8_forward`
+      core::arch::asm! {
+        "1:",
+        "subs    {count}, {count}, #2",
+        "ldrhge  {temp}, [{src}], #2",
+        "strhge  {temp}, [{dest}], #2",
+        "bgt     1b",
+        dest = inout(reg) dest,
+        src = inout(reg) src,
+        count = inout(reg) count,
+        temp = out(reg) _,
+        options(nostack)
+      }
+    }
+    no: {
+      while count >= 2 {
+        *dest = *src;
+        dest = dest.add(1);
+        src = src.add(1);
+        count -= 2;
+      }
+    }
+  }
+  eprintln!("post-loop count: {count}");
+  if (count & 1) != 0 {
+    eprintln!("copying 1 byte");
+    let dest = dest.cast::<mu_u8>();
+    let src = src.cast::<mu_u8>();
+    *dest = *src;
+    // skip adjusting count for the last byte, it's the end of the fn
   }
 }
 
@@ -92,13 +160,11 @@ pub unsafe extern "C" fn copy_u8_forward(
 pub unsafe extern "C" fn copy_u8_backward(
   mut dest: *mut mu_u8, mut src: *const mu_u8, mut count: usize,
 ) {
+  // IMPORTANT: in the backward loop we adjust the pointers *before* the copy,
+  // instead of after the copy like the forward loop does.
   cfg_armv4t! {
     yes: {
-      // This loop assumes that the count is non-zero to start, and so it does
-      // the subtract followed by a conditional copy and continue.
-      // * Pro: 8 bytes less code in the binary
-      // * Pro: save 2 cycles on non-zero sized copies
-      // * Con: lose 3 cycles on zero sized copies.
+      // The loop reasoning here is similar to `copy_u8_forward`
       core::arch::asm! {
         "1:",
         "subs    {count}, {count}, #1",
@@ -113,16 +179,71 @@ pub unsafe extern "C" fn copy_u8_backward(
       }
     }
     no: {
-      // When we write the loop as Rust, LLVM will generally test for 0 and
-      // early return, then copy and subtract and continue.
-      while count > 0 {
-        // IMPORTANT: in the backward loop we subtract *before* the copy,
-        // instead of after the copy like the forward loop does.
+      while count >= 1 {
         dest = dest.sub(1);
         src = src.sub(1);
         *dest = *src;
         count -= 1;
       }
     }
+  }
+}
+
+/// Copies `count` bytes from `src` to `dest`, starting at one-past-the-end.
+///
+/// Copies are done in 2-byte chunks as much as possible. If the number of bytes
+/// to copy is odd then the last copy will be a single-byte copy.
+///
+/// ## Safety
+/// * If `count` is zero, the `src` and `dest` pointers are not accessed, and
+///   they can even be invalid or null.
+/// * If `count` is non-zero, then both `src` and `dest` must be aligned, be the
+///   one-past-the-end pointers for `count` bytes backward, and one of the
+///   following must be true:
+///   * The `src` and `dest` regions are entirely disjoint.
+///   * `src` equals `dest` (there is exact overlap).
+///   * `src` is *less* than `dest` (a partial overlap).
+#[inline]
+#[cfg_attr(feature = "link_iwram", link_section = ".iwram.copy_u8_backward")]
+#[cfg_attr(
+  all(target_arch = "arm", target_feature = "thumb-mode", feature = "armv4t"),
+  instruction_set(arm::a32)
+)]
+pub unsafe extern "C" fn copy_u16_backward(
+  mut dest: *mut mu_u16, mut src: *const mu_u16, mut count: usize,
+) {
+  // IMPORTANT: in the backward loop we adjust the pointers *before* the copy,
+  // instead of after the copy like the forward loop does.
+  cfg_armv4t! {
+    yes: {
+      // The loop reasoning here is similar to `copy_u8_forward`
+      core::arch::asm! {
+        "1:",
+        "subs    {count}, {count}, #2",
+        "ldrhge  {temp}, [{src}, #-2]!",
+        "strhge  {temp}, [{dest}, #-2]!",
+        "bgt     1b",
+        dest = inout(reg) dest,
+        src = inout(reg) src,
+        count = inout(reg) count,
+        temp = out(reg) _,
+        options(nostack)
+      }
+    }
+    no: {
+      while count >= 2 {
+        dest = dest.sub(1);
+        src = src.sub(1);
+        *dest = *src;
+        count -= 2;
+      }
+    }
+  }
+  if count != 0 {
+    let mut dest = dest.cast::<mu_u8>();
+    let mut src = src.cast::<mu_u8>();
+    dest = dest.sub(1);
+    src = src.sub(1);
+    *dest = *src;
   }
 }
--- tests/all_tests.rs
+++ tests/all_tests.rs
@@ -1,8 +1,13 @@
-use aeabi_fns::{copy_u8_backward, copy_u8_forward};
+use aeabi_fns::{
+  copy_u16_backward, copy_u16_forward, copy_u8_backward, copy_u8_forward,
+};
+
+// Note(Lokathor): Different base types on the vecs to get different minimum
+// alignments on the buffer.
 
 #[allow(dead_code)]
 fn rand_bytes(n: usize) -> Vec<u8> {
-  let mut v = vec![0; n];
+  let mut v = vec![0_u8; n];
   getrandom::getrandom(&mut v).unwrap();
   v
 }
@@ -81,7 +86,7 @@ fn test_copy_u8_forward() {
   }
 
   // src > dest works even when the regions overlap
-  for len in 3..=16_usize {
+  for len in 0..=16_usize {
     let src = rand_bytes(64);
     for s in 0..len {
       for d in 0..len {
@@ -153,7 +158,7 @@ fn test_copy_u8_backward() {
   }
 
   // src > dest works even when the regions overlap
-  for len in 3..=16_usize {
+  for len in 0..=16_usize {
     let src = rand_bytes(64);
     for s in 0..len {
       for d in 0..len {
@@ -164,13 +169,179 @@ fn test_copy_u8_backward() {
         let mut out_actual = src.clone();
         let b = (lcg.next_u32() % 16) as usize;
         let d_start = b + d;
-        //let d_end = d_start + len;
         let s_start = b + s;
         let s_end = s_start + len;
         out_expected.copy_within(s_start..s_end, d_start);
         unsafe {
           let p = out_actual.as_mut_ptr();
           copy_u8_backward(
+            p.add(d_start).add(len).cast(),
+            p.add(s_start).add(len).cast(),
+            len,
+          )
+        }
+        assert_eq!(out_expected, out_actual);
+      }
+    }
+  }
+}
+
+#[test]
+fn test_copy_u16_forward() {
+  let mut lcg = Lcg::new();
+
+  // disjoint regions is the basic use case
+  for len in 0..=16_usize {
+    let src = rand_halfwords(64);
+    for s in 0..len {
+      for d in 0..len {
+        let mut dest_expected = vec![0_u16; 64];
+        let mut dest_actual = vec![0_u16; 64];
+        let b = (lcg.next_u32() % 16) as usize;
+        let d_start = b + d;
+        let s_start = b + s;
+        // assume `copy_u8_forward` already works and build our "expected" value
+        // using it, which allows us to do copies of odd lengths.
+        unsafe {
+          copy_u8_forward(
+            dest_expected.as_mut_ptr().add(d_start).cast(),
+            src.as_ptr().add(s_start).cast(),
+            len,
+          )
+        }
+        unsafe {
+          copy_u16_forward(
+            dest_actual.as_mut_ptr().add(d_start).cast(),
+            src.as_ptr().add(s_start).cast(),
+            len,
+          )
+        }
+        assert_eq!(dest_expected, dest_actual, "len:{len}");
+      }
+    }
+  }
+
+  // src == dest is allowed for simplicity (but has no effect)
+  for len in 0..=16_usize {
+    let base = rand_halfwords(64);
+    for s in 0..len {
+      let mut new = base.clone();
+      unsafe {
+        let p = new.as_mut_ptr();
+        copy_u16_forward(p.add(s).cast(), p.add(s).cast(), len)
+      }
+      assert_eq!(base, new);
+    }
+  }
+
+  // src > dest works even when the regions overlap
+  for len in 3..=16_usize {
+    let src = rand_halfwords(64);
+    for s in 0..len {
+      for d in 0..len {
+        if s < d {
+          continue;
+        }
+        let mut out_expected = src.clone();
+        let mut out_actual = src.clone();
+        let b = (lcg.next_u32() % 16) as usize;
+        let d_start = b + d;
+        let s_start = b + s;
+        // assume `copy_u8_forward` already works and build our "expected" value
+        // using it, which allows us to do copies of odd lengths.
+        unsafe {
+          copy_u8_forward(
+            out_expected.as_mut_ptr().add(d_start).cast(),
+            src.as_ptr().add(s_start).cast(),
+            len,
+          )
+        }
+        unsafe {
+          let p = out_actual.as_mut_ptr();
+          copy_u16_forward(p.add(d_start).cast(), p.add(s_start).cast(), len)
+        }
+        assert_eq!(out_expected, out_actual);
+      }
+    }
+  }
+}
+
+#[test]
+fn test_copy_u16_backward() {
+  let mut lcg = Lcg::new();
+
+  // disjoint regions is the basic use case
+  for len in 0..=16_usize {
+    let src = rand_halfwords(64);
+    for s in 0..len {
+      for d in 0..len {
+        let mut dest_expected = vec![0_u16; 64];
+        let mut dest_actual = vec![0_u16; 64];
+        let b = (lcg.next_u32() % 16) as usize;
+        let d_start = b + d;
+        let s_start = b + s;
+        // assume `copy_u8_backward` works and use it so we can have odd lengths
+        unsafe {
+          copy_u8_backward(
+            dest_expected.as_mut_ptr().add(d_start).add(len).cast(),
+            src.as_ptr().add(s_start).add(len).cast(),
+            len,
+          )
+        }
+        unsafe {
+          copy_u16_backward(
+            dest_actual.as_mut_ptr().add(d_start).add(len).cast(),
+            src.as_ptr().add(s_start).add(len).cast(),
+            len,
+          )
+        }
+        assert_eq!(dest_expected, dest_actual);
+      }
+    }
+  }
+
+  // src == dest is allowed for simplicity (but has no effect)
+  for len in 0..=16_usize {
+    let base = rand_halfwords(64);
+    for s in 0..len {
+      let mut new = base.clone();
+      unsafe {
+        let p = new.as_mut_ptr();
+        copy_u16_backward(
+          p.add(s).add(len).cast(),
+          p.add(s).add(len).cast(),
+          len,
+        )
+      }
+      assert_eq!(base, new);
+    }
+  }
+
+  // src > dest works even when the regions overlap
+  for len in 0..=16_usize {
+    let src = rand_bytes(64);
+    for s in 0..len {
+      for d in 0..len {
+        if s > d {
+          continue;
+        }
+        let mut out_expected = src.clone();
+        let mut out_actual = src.clone();
+        let b = (lcg.next_u32() % 16) as usize;
+        let d_start = b + d;
+        let s_start = b + s;
+        // assume `copy_u8_backward` works and use it so we can have odd lengths
+        unsafe {
+          let p = out_expected.as_mut_ptr();
+          copy_u8_backward(
+            p.add(d_start).add(len).cast(),
+            p.add(s_start).add(len).cast(),
+            len,
+          )
+        }
+        unsafe {
+          let p = out_actual.as_mut_ptr();
+          copy_u16_backward(
             p.add(d_start).add(len).cast(),
             p.add(s_start).add(len).cast(),
             len,
